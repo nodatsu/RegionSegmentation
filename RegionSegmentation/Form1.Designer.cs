4ec6b301da1fcf88e48a34a752779c84a74c753a
H.Tsunoda <tsunoda@aomori-u.ac.jp>
2014-09-04 19:03:08 +0900

    最初のコミット

---
+++ RegionSegmentation/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace RegionSegmentation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.読み込みToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルToolStripMenuItem
+            // 
+            this.ファイルToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.読み込みToolStripMenuItem});
+            this.ファイルToolStripMenuItem.Name = "ファイルToolStripMenuItem";
+            this.ファイルToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ファイルToolStripMenuItem.Text = "ファイル";
+            // 
+            // 読み込みToolStripMenuItem
+            // 
+            this.読み込みToolStripMenuItem.Name = "読み込みToolStripMenuItem";
+            this.読み込みToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.読み込みToolStripMenuItem.Text = "読み込み";
+            this.読み込みToolStripMenuItem.Click += new System.EventHandler(this.読み込みToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(284, 237);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            this.pictureBox1.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseWheel);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ファイルToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 読み込みToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ RegionSegmentation/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegionSegmentation
+{
+    public partial class Form1 : Form
+    {
+        // 表示用
+        Bitmap outputImage;
+        double outputZoom;
+        Point outputShift;
+        bool isMouseDrag;
+        Point mousePre;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 読み込みToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                // Matの準備
+                OpenCvSharp.CPlusPlus.Mat matSrc = new OpenCvSharp.CPlusPlus.Mat(dialog.FileName);
+                OpenCvSharp.CPlusPlus.Mat matDst = matSrc.Clone();
+
+                //// 画像ピラミッドを用いた画像の領域分割 
+                //int level = 4;              // ピラミッドレベル
+                //double threshold1 = 128;    // ピクセルを接続する閾値
+                //double threshold2 = 50;     // クラスタリングの範囲の閾値
+
+                //// IplImageの準備(C API用)
+                //OpenCvSharp.IplImage iplSrc = matSrc.ToIplImage();
+                //OpenCvSharp.IplImage iplDst = iplSrc.Clone();
+
+                //// ピラミッド画像作成のためのROI設定(2^levelで割り切れるサイズ)
+                //OpenCvSharp.CvRect roi;
+                //roi.X = 0;
+                //roi.Y = 0;
+                //roi.Width = iplSrc.Width & -(1 << level);
+                //roi.Height = iplSrc.Height & -(1 << level);
+                //iplSrc.SetROI(roi);
+                //iplDst.SetROI(roi);
+
+                //OpenCvSharp.Cv.PyrSegmentation(iplSrc, iplDst, level, threshold1, threshold2);
+
+                //// IplImage -> Matに戻す
+                //matDst = new OpenCvSharp.CPlusPlus.Mat(iplDst);
+
+                //// 平均値シフト法による画像のセグメント化
+                //double sp = 30; // 空間窓の半径
+                //double sr = 30; // 色空間窓の半径
+                //int level = 4;  // セグメンテーションに用いるピラミッドの最大レベル
+                //OpenCvSharp.CPlusPlus.TermCriteria term = new OpenCvSharp.CPlusPlus.TermCriteria(OpenCvSharp.CriteriaType.Iteration, 5, 1);
+                ////OpenCvSharp.CPlusPlus.TermCriteria term = new OpenCvSharp.CPlusPlus.TermCriteria(OpenCvSharp.CriteriaType.Epsilon, 5, 1);
+
+                //OpenCvSharp.CPlusPlus.Cv2.PyrMeanShiftFiltering(matSrc, matDst, sp, sr, level, term);
+
+                // Watershedアルゴリズムによる画像の領域分割 
+
+                // IplImageの準備(C API用)
+                OpenCvSharp.IplImage iplSrc = matSrc.ToIplImage();
+                OpenCvSharp.IplImage iplDst = iplSrc.Clone();
+
+                // マーカ画像の準備
+                OpenCvSharp.IplImage iplMarker = new OpenCvSharp.IplImage(iplSrc.Size, OpenCvSharp.BitDepth.S32, 1);
+                iplMarker.Zero();
+
+                // マーカ設置(等分割)
+                int wdiv = 10;   // 分割数(横)
+                int hdiv = 10;   // 分割数(縦)
+                OpenCvSharp.CvPoint[,] mpt = new OpenCvSharp.CvPoint[wdiv, hdiv];
+                for (int i = 0; i < wdiv; i++)
+                {
+                    for (int j = 0; j < hdiv; j++)
+                    {
+                        mpt[i, j] = new OpenCvSharp.CvPoint((int)(iplSrc.Width / wdiv * (i + 0.5)), (int)(iplSrc.Height / hdiv * (j + 0.5)));
+                        iplMarker.Circle(mpt[i, j], 5, OpenCvSharp.CvScalar.ScalarAll(i * wdiv + j), OpenCvSharp.Cv.FILLED, OpenCvSharp.LineType.Link8, 0);
+                    }
+                }
+
+                // 分割実行
+                OpenCvSharp.Cv.Watershed(iplSrc, iplMarker);
+
+                // マーカの描画
+                for (int i = 0; i < wdiv; i++)
+                {
+                    for (int j = 0; j < hdiv; j++)
+                    {
+                        iplDst.Circle(mpt[i, j], 20, OpenCvSharp.CvColor.White, 3, OpenCvSharp.LineType.Link8, 0);
+                    }
+                }
+
+                // 領域境界の描画
+                for (int i = 0; i < iplMarker.Height; i++)
+                {
+                    for (int j = 0; j < iplMarker.Width; j++)
+                    {
+                        int idx = (int)(iplMarker.Get2D(i, j).Val0);
+                        if (idx == -1)
+                        {
+                            iplDst.Set2D(i, j, OpenCvSharp.CvColor.Red);
+                        }
+                    }
+                }
+
+                // IplImage -> Matに戻す
+                matDst = new OpenCvSharp.CPlusPlus.Mat(iplDst);
+
+                this.outputImage = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(matDst);
+
+                this.outputZoom = 1.0;
+                this.outputShift = new Point(0, 0);
+                this.isMouseDrag = false;
+                this.mousePre = new Point(0, 0);
+
+                this.pictureBox1.Invalidate();
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // ボタン識別
+		    if (!(e.Button == MouseButtons.Left))
+            {
+                return;
+            }
+
+            // マウスの元位置設定
+            this.mousePre.X = e.X;
+            this.mousePre.Y = e.Y;
+
+            // マウス移動状態設定
+            this.isMouseDrag = true;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!this.isMouseDrag)
+            {
+                return;
+            }
+
+            // シフト量
+            this.outputShift.X += (e.X - this.mousePre.X);
+            this.outputShift.Y += (e.Y - this.mousePre.Y);
+
+            // マウスの元位置更新
+            this.mousePre.X = e.X;
+            this.mousePre.Y = e.Y;
+
+            // 再描画
+            this.pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+      		// ボタン識別
+		    if (!(e.Button == MouseButtons.Left))
+            {
+                return;
+            }
+
+		    // マウス移動状態解除
+		    this.isMouseDrag = false;
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            // マウスカーソルが来たらフォーカス(これをしないとホイールイベントが拾えない)
+            pictureBox1.Focus();
+        }
+
+        private void pictureBox1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            // 現在のズーム値を記録
+            double zoomPre = this.outputZoom;
+
+            // 1カウントで10%増減、最小100%、最大1000%
+            this.outputZoom += 0.1 * this.outputZoom * e.Delta / 120;   // マウスホイールの1カウント(Delta)は120
+            this.outputZoom = Math.Min(10.0, Math.Max(1.0, this.outputZoom));
+
+            // シフト値の更新
+            this.outputShift.X = (int)((this.outputShift.X - e.X) * this.outputZoom / zoomPre + e.X);
+            this.outputShift.Y = (int)((this.outputShift.Y - e.Y) * this.outputZoom / zoomPre + e.Y);
+
+            // ズームが1.0ならシフト値リセット
+            if (Math.Abs(this.outputZoom - 1.0) < 0.001)
+            {
+                this.outputShift.X = 0;
+                this.outputShift.Y = 0;
+            }
+
+            // 再描画
+            this.pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (this.outputImage != null)
+            {
+                // アスペクトとスケール
+                double aspect = (double)this.outputImage.Width / this.outputImage.Height;
+                double scale = Math.Min((double)((PictureBox)sender).Height, ((PictureBox)sender).Width / aspect) * this.outputZoom;
+
+                // 描画
+                e.Graphics.DrawImage(this.outputImage, this.outputShift.X, this.outputShift.Y, (int)(scale * aspect), (int)scale);
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            this.pictureBox1.Invalidate();
+        }
+    }
+}
